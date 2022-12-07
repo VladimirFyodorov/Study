cced0ad6b8fa952abe391575a0689df4ebf3bc1e
VladimirFyodorov <66209119+VladimirFyodorov@users.noreply.github.com>
2022-12-07 12:44:51 +0600

    fix 11

--- Functions/Solutions/Task_11.js
+++ Functions/Solutions/Task_11.js
@@ -1,6 +1,4 @@
 const smallestDivisor = (num) => {
-	if (num === 1) return 1;
-
 	const iter = (div) => {
 		if (div**2 > num) return num;
 		if (num % div === 0) return div;
@@ -10,5 +8,6 @@ const smallestDivisor = (num) => {
 	return iter(2);
 };
 
+console.log(smallestDivisor(1)); // 1
 console.log(smallestDivisor(15)); // 3
 console.log(smallestDivisor(17)); // 17
