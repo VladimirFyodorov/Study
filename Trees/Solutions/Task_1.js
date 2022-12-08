484059bf511592d780194875b554a48209b346ed
VladimirFyodorov <66209119+VladimirFyodorov@users.noreply.github.com>
2022-12-08 23:12:37 +0600

    Task 1

---
+++ Trees/Solutions/Task_1.js
@@ -0,0 +1,13 @@
+const removeFirstLevel = (tree) => {
+	return tree
+		.filter(subtree => Array.isArray(subtree))
+		.flat();
+};
+
+// Второй уровень тут: 5, 3, 4
+const tree1 = [[5], 1, [3, 4]];
+console.log(removeFirstLevel(tree1)); // [5, 3, 4]
+ 
+const tree2 = [1, 2, [3, 5], [[4, 3], 2]];
+console.log(removeFirstLevel(tree2));
+// [3, 5, [4, 3], 2]
