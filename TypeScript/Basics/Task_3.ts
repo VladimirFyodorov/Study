dfae97c545f311653f15273ba4755b961029ad94
VladimirFyodorov <66209119+VladimirFyodorov@users.noreply.github.com>
2023-02-01 19:57:06 +0600

    Task 3

---
+++ TypeScript/Basics/Task_3.ts
@@ -0,0 +1,9 @@
+function repeat(text: string, count: number) {
+  // BEGIN (write your solution here)
+  let res = '';
+  for (let i = 0; i < count; i++) {
+    res += text;
+  }
+  return res;
+  // END
+}
