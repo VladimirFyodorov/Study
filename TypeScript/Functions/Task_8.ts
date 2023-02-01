bf0e0b5e8004dd31973d69b05b4dd8874bc8d36e
VladimirFyodorov <66209119+VladimirFyodorov@users.noreply.github.com>
2023-02-01 21:52:09 +0600

    Task 8 alt

--- TypeScript/Functions/Task_8.ts
+++ TypeScript/Functions/Task_8.ts
@@ -7,3 +7,15 @@ const sayHello: Overloaded = (fname: string, lname?: string): string => {
   if (lname === undefined) return `Hi ${fname}`;
   return `Hello ${fname} ${lname}`;
 }
+
+// ---------- or ---------- //
+
+function sayHello2(name: string): string;
+function sayHello2(firstName: string, lastName: string): string;
+function sayHello2(firstName: string, lastName?: string): string {
+  if (lastName === undefined) {
+    return `Hi ${firstName}`;
+  }
+
+  return `Hello ${firstName} ${lastName}`;
+}
