4528dfcb7ec047f7073caaf849053be292796f59
VladimirFyodorov <66209119+VladimirFyodorov@users.noreply.github.com>
2023-02-02 10:34:50 +0600

    Task 3

---
+++ TypeScript/3_Arrays/Task_3.ts
@@ -0,0 +1,8 @@
+function reverse(coll: ReadonlyArray<number>): Array<number> {
+  const init: number[] = [];
+
+  return coll.reduce((acc, value, index) => {
+    acc[coll.length - index - 1] = value;
+    return acc;
+  }, init);
+}
